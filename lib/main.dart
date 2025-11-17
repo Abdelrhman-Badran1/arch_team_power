7e8d03e23c6b53bf3a2234f2f8befec1aa8ed705
gayarjr <mahmodashr@gmail.com>
2025-11-17 23:01:05 +0200

    Edit

--- lib/main.dart
+++ lib/main.dart
@@ -41,3 +41,51 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+// الكود دا تبعي بشغلله عندي محدش يمسحه او يعدل عليه حاجه
+// import 'package:arch_team_power/core/routes/app_router.dart';
+// import 'package:arch_team_power/features/Notifications/presentation/screens/notifications_page.dart';
+// import 'package:arch_team_power/generated/l10n.dart';
+// import 'package:device_preview/device_preview.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_localizations/flutter_localizations.dart';
+// import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+// void main() {
+//   runApp(DevicePreview(enabled: true, builder: (context) => const MyTestApp()));
+// }
+
+// class MyTestApp extends StatelessWidget {
+//   const MyTestApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return ScreenUtilInit(
+//       designSize: const Size(324.77, 674.65),
+//       minTextAdapt: true,
+//       splitScreenMode: true,
+//       builder: (context, child) {
+//         return MaterialApp(
+//           useInheritedMediaQuery: true,
+//           debugShowCheckedModeBanner: false,
+//           locale: DevicePreview.locale(context),
+
+//           // DevicePreview builder
+//           builder: DevicePreview.appBuilder,
+
+//           // Localization setup
+//           localizationsDelegates: const [
+//             S.delegate,
+//             GlobalMaterialLocalizations.delegate,
+//             GlobalWidgetsLocalizations.delegate,
+//             GlobalCupertinoLocalizations.delegate,
+//           ],
+//           supportedLocales: S.delegate.supportedLocales,
+
+//           // Home Page (زي ما انت عايز)
+//           home: const NotificationsPage(),
+//         );
+//       },
+//     );
+//   }
+// }
