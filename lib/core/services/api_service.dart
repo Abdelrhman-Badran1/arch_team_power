731d8b8b85864efba60a856c8b54ba14f1dc6932
mohammed sabri <msabriyassin@gmail.com>
2025-12-16 11:16:19 +0200

    add api service

--- lib/core/services/api_service.dart
+++ lib/core/services/api_service.dart
@@ -1,1 +1,12 @@
+import 'package:dio/dio.dart';
 
+class ApiService {
+  final Dio dio;
+  final baseUrl = "https://archtech.test-trifhi.com/api/";
+
+  ApiService(this.dio);
+  Future<Map<String, dynamic>> get({required String endPoint}) async {
+    var response = await dio.get('$baseUrl$endPoint');
+    return response.data;
+  }
+}
